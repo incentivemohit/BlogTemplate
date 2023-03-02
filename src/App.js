6d66fad983f02455b56263a64e9082f50dd9347c
mkcoder597 <86621804+incentivemohit@users.noreply.github.com>
2023-03-02 19:49:22 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/Home/Header/About";
 import Contact from "./components/Home/Header/Contact";
 import Home from "./components/Home/Home";
